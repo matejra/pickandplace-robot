e7f6cf2263dd9a47da97e5a174e3a5005645c2ab
Matej Razingar <matejrazingar@yahoo.com>
2018-12-05 15:21:51 +0100

    solving usb_cam submodule

--- camera_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/matej/camera_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/matej/camera_ws/devel;/opt/ros/kinetic".split(';') if "/home/matej/camera_ws/devel;/opt/ros/kinetic" != "" else []
